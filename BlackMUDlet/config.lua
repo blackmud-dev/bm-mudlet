fc9f8d476336f0c779ad1ab4ad38a8bc8c0b37d9
yetanotherkevin <yetanotherkevin@gmail.com>
2023-05-15 23:51:51 -0400

    Initial Commit

---
+++ BlackMUDlet/config.lua
@@ -0,0 +1,7 @@
+mpackage = [[BlackMUDlet]]
+author = [[yetanotherkevin]]
+icon = [[icon.jpg]]
+title = [[Mudlet Server Package for BlackMUD]]
+description = [[TODO]]
+version = [[1]]
+created = "2023-05-15T23:17:55-04:00"
